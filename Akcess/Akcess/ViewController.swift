93b7c1da15365d999b909d27f97035592b8e1af4
floatingkernal <salman.sharif@mail.utoronto.ca>
2017-01-21 02:08:37 -0500

    added Akcess

---
+++ Akcess/Akcess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Akcess
+//
+//  Created by Salman Sharif on 2017-01-21.
+//  Copyright © 2017 Salman Sharif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
